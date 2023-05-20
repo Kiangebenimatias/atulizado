526906a1e7196628f20573fe3087c34e819578c5
Kiangebeni Matias <129292053+Kiangebenimatias@users.noreply.github.com>
2023-05-20 22:58:26 +0100

    pagina inicial

---
+++ login/javascript/programa.js
@@ -0,0 +1,9 @@
+
+let usuario = document.querySelector("#id-email").val()
+let senha = document.querySelector("#id-senha").val()
+
+function Entrar() {
+    alert("Ok")
+
+    window.location.href = "/paginainicial/index.html"
+}
